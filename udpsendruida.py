5fcf11219c184b1c7505fc7f2aaca1beb458b98c
Juergen Weigert <jw@owncloud.com>
2018-04-22 17:30:59 +0200

    retrying the first chunk only. Retrying with truncated exponential backoff to be nice to a congested network...

--- udpsendruida.py
+++ udpsendruida.py
@@ -8,6 +8,20 @@
 # * https://wiki.fablab-nuernberg.de/w/Diskussion:Nova_35
 # * https://github.com/kkaempf/LibLaserCut/tree/thunderlaser/src/com/t_oster/liblasercut/drivers/ruida
 #
+#
+# Protocol specifications:
+# - The device listens on a fixed UDP port (DEST_PORT). IPaddress is configurable, netmask is 255.255.255.0 fixed.
+# - An RD file is transfered as payload, same commands and syntax as with USB-Serial or USB-MassStorage.
+# - The payload is split in chunks with a well known maximum size (MTU). (The last packet is usually shorter)
+# - There is no header, and no arbitration phase, but successful transmission of the first chunk indicates device ready.
+# - Each chunk starts with a two byte checksum, followed by payload data. Length of the payload is implicit by the
+#   UDP datagram size. (Would not work with TCP)
+# - Each chunk is acknowledged with a single byte response packet:
+#   0xc6 if all is well, The next chunk should be sent. TODO: Within a timout?
+#   0x46 if error. TODO: Checksum error and/or busy?
+# - The first chunk should be retried when 0x46 was received. For subsequent chunks transmission should be aborted.
+# - TODO: is a pause needed after the last chunk before the next paylod can be sent?
+#
 # test against:
 # ncat -l -u -v 50200
 
@@ -52,10 +66,12 @@ class RuidaUdp():
         chunk_sz = self.MTU
       chksum = self._checksum(data, start, chunk_sz)
       buf = chksum + data[start:start+chunk_sz]
-      self.send(buf)
+      self.send(buf, retry=(start == 0))
       start += chunk_sz
 
-  def send(self, ary):
+  def send(self, ary, retry=False):
+    retry_delay_sec = 0.2
+    retry_delay_sec_max = 5.0
     while True:
       self.sock.send(ary)
       try:
@@ -69,7 +85,12 @@ class RuidaUdp():
         break
       # l == 1
       if data[0] == 0x46:           # 'F'
-        raise IOError("checksum error")
+        if retry:
+          print("retrying ...")
+          time.sleep(retry_delay_sec)   # truncated binary backoff
+          if retry_delay_sec < retry_delay_sec_max: retry_delay_sec *= 2
+        else:
+          raise IOError("checksum error")
       elif data[0] == 0xc6:
         # print("received ACK");
         break
