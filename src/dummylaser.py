34985097ba3173bf5a411db0dbdf2b026b2d9d2a
Jürgen Weigert <jw@owncloud.com>
2019-02-12 02:13:32 +0100

    added a dummylaser.py to make sent commands visible.

---
+++ src/dummylaser.py
@@ -0,0 +1,64 @@
+#! /usr/bin/python3
+#
+# thunderlaserdummy.py
+#
+# Prepare a loopback network like this:
+#   ifconfig lo:1 192.168.2.1 up
+#
+
+from __future__ import print_function
+
+import socket, sys
+
+ip_addr = '192.168.2.21'
+server_port = 50200
+mtu = 1470
+
+def unscramble(b):
+    """ unscramble a single byte for reading from *.rd files """
+    res_b=b-1
+    if res_b<0: res_b+=0x100
+    res_b^=0x88
+    fb=res_b&0x80
+    lb=res_b&1
+    res_b=res_b-fb-lb
+    res_b|=lb<<7
+    res_b|=fb>>7
+    return res_b
+
+def unscramble_bytes(data):
+    if sys.version_info.major < 3:
+      return bytes([unscramble(ord(b)) for b in data])
+    else:
+      return bytes([unscramble(b) for b in data])
+
+def check_checksum(data):
+  sum = 0
+  for i in bytes(data[2:]):
+    sum += i & 0xff     # unsigned
+  seen = ((data[0] & 0xff) << 8) + (data[1] & 0xff)
+  if seen == sum:
+    return data[2:]
+  return None
+
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+if hasattr(socket, "SO_REUSEPORT"):
+  sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+sock.bind((ip_addr, server_port))
+
+# somebody does 
+#   sock.sendto(b"Hello World\n", (ip_addr, server_port))
+
+
+while True:
+    data, addr = sock.recvfrom(1024)
+    buf = check_checksum(data)
+    if buf is None:
+      print("bad checksum in %s from %s" % (data, addr))
+      sock.sendto(b'\x46', addr)     # reply 0x46=NACK
+    else:
+      buf = unscramble_bytes(buf)
+      print("Seen: %s from %s" % (buf, addr))
+      sock.sendto(b'\xc2', addr)     # reply 0xc2=ACK
